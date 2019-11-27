f13c9c22dc768b49b87beccf7cf2df75b3e903fd
Tien93-iqvc <58262966+Tien93-iqvc@users.noreply.github.com>
2019-11-27 21:41:02 +0700

    Share

---
+++ bait01/index.php
@@ -0,0 +1,35 @@
+<?php require "header.php"; ?>
+<div class="group-box">
+	<div align="center">
+	<div class="title">Thông báo</div>
+	
+	 <h3>Chào mừng bạn đến Trang Quản Lý Nội Trú.</h3>
+	 <h3>Trang web được phát triển bởi Team IQVôCực.</h3>  
+	 <p>
+	 </div>
+</div>
+
+<div class="group-box">
+	<div align="center">
+	<div class="title">Thông báo</div>
+	
+	 <h3>Ngày 22//11//2019 , Trường sẽ tổ chức đăng ký phòng cho các tòa nhà. A1, C1, C2, C3</h3> 
+	 <h3>Vui lòng các sinh viên xem thông báo đăng ký phòng đúng thời hạn.</h3>
+	 
+	 
+	 <p>
+	 </div>
+</div>
+
+<div id="hello">
+	<div class="group-box">
+	<div align="center">
+	<div class="title">Thông báo </div>
+	
+	 <h3>Hạn đăng ký đến hết ngày 30/11/2019.</h3>
+	 <h3>............Hết!...........</h3>  
+	 <p>
+	 </div>
+</div>
+	
+</div>
